28427dfd76ee7a24b3fdbdfe9d549289244733fe
ibanunmangun <62372980+ibanunmangun@users.noreply.github.com>
2021-08-05 18:03:59 +0800

    Add files via upload

---
+++ data/PointofInterestBanjar_2.js
@@ -0,0 +1,23 @@
+var json_PointofInterestBanjar_2 = {
+"type": "FeatureCollection",
+"name": "PointofInterestBanjar_2",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "name": "Bukit Sikunir Dieng", "desc": "Sikunir, Dieng, kebun & Hutan, Sembungan, Kejajar, Wonosobo Regency, Central Java 56354, Indonesia", "X": null, "Y": null, "Info": "point of interest" }, "geometry": { "type": "Point", "coordinates": [ 109.9247722, -7.2391295 ] } },
+{ "type": "Feature", "properties": { "name": "Telaga Cebong", "desc": "Telaga Cebong, Sawah & Hutan, Sembungan, Kejajar, Wonosobo Regency, Central Java, Indonesia", "X": null, "Y": null, "Info": "point of interest" }, "geometry": { "type": "Point", "coordinates": [ 109.92, -7.2361111 ] } },
+{ "type": "Feature", "properties": { "name": "Dieng Plateau Theater", "desc": "Dieng Plateau Theatre, Dieng, Kejajar, Wonosobo Regency, Central Java 56354, Indonesia", "X": null, "Y": null, "Info": "point of interest" }, "geometry": { "type": "Point", "coordinates": [ 109.914689, -7.216655 ] } },
+{ "type": "Feature", "properties": { "name": "Telaga Warna", "desc": "Telaga Warna, Dieng, Dieng, Kejajar, Kabupaten Wonosobo, Jawa Tengah 56354, Indonesia", "X": null, "Y": null, "Info": "point of interest" }, "geometry": { "type": "Point", "coordinates": [ 109.9137591, -7.2147759 ] } },
+{ "type": "Feature", "properties": { "name": "Kawah Sikidang", "desc": "Sikidang Crater, Bakal Buntu, Dieng Kulon, Batur, Banjarnegara, Central Java 53456, Indonesia", "X": null, "Y": null, "Info": "point of interest" }, "geometry": { "type": "Point", "coordinates": [ 109.9050214, -7.2201011 ] } },
+{ "type": "Feature", "properties": { "name": "Bukit Scooter Dieng", "desc": "Bukit Scooter Dieng, Simpangan, Dieng, Batur, Banjarnegara, Jawa Tengah 53456, Indonesia", "X": null, "Y": null, "Info": "point of interest" }, "geometry": { "type": "Point", "coordinates": [ 109.9061724, -7.1973296 ] } },
+{ "type": "Feature", "properties": { "name": "Candi Arjuna", "desc": "Arjuna Temple, Karangsari, Dieng Kulon, Batur, Banjarnegara, Central Java 53456, Indonesia", "X": null, "Y": null, "Info": "point of interest" }, "geometry": { "type": "Point", "coordinates": [ 109.9069386, -7.2050346 ] } },
+{ "type": "Feature", "properties": { "name": "Tuk Bimolukar", "desc": "Tuk Bimolukar, Kalilembu, Dieng, Kejajar, Wonosobo Regency, Central Java 56354, Indonesia", "X": null, "Y": null, "Info": "point of interest" }, "geometry": { "type": "Point", "coordinates": [ 109.9129018, -7.204561 ] } },
+{ "type": "Feature", "properties": { "name": "IGM Shop Pusat Carica dan Oleh Oleh Khas Dieng Wonosobo", "desc": "IGM Shop Pusat Carica dan Oleh Oleh Khas Dieng Wonosobo, Parikesit, Kejajar, Wonosobo Regency, Central Java 56354, Indonesia", "X": null, "Y": null, "Info": "point of interest" }, "geometry": { "type": "Point", "coordinates": [ 109.9279619, -7.2143469 ] } },
+{ "type": "Feature", "properties": { "name": "Rm Hd Gardu Pandang", "desc": "Rm Hd Gardu Pandang, Jl. Dieng, Sidorejo, Tieng, Kejajar, Kabupaten Wonosobo, Jawa Tengah 56354, Indonesia", "X": null, "Y": null, "Info": "point of interest" }, "geometry": { "type": "Point", "coordinates": [ 109.9382983, -7.2333165 ] } },
+{ "type": "Feature", "properties": { "name": "PKU Muhammadiyah Dieng", "desc": null, "X": null, "Y": null, "Info": "public facilities" }, "geometry": { "type": "Point", "coordinates": [ 109.943197, -7.233985 ] } },
+{ "type": "Feature", "properties": { "name": "Puskesmas Batur II", "desc": null, "X": null, "Y": null, "Info": "public facilities" }, "geometry": { "type": "Point", "coordinates": [ 109.902673, -7.202922 ] } },
+{ "type": "Feature", "properties": { "name": "Pos Polisi Dieng", "desc": null, "X": null, "Y": null, "Info": "public facilities" }, "geometry": { "type": "Point", "coordinates": [ 109.90887, -7.202513 ] } },
+{ "type": "Feature", "properties": { "name": "SPBU Pertamina 44 534 04", "desc": null, "X": null, "Y": null, "Info": "public facilities" }, "geometry": { "type": "Point", "coordinates": [ 109.902438, -7.202907 ] } },
+{ "type": "Feature", "properties": { "name": "Mushola Al Amin - Ridlo Home Stay", "desc": null, "X": null, "Y": null, "Info": "public facilities" }, "geometry": { "type": "Point", "coordinates": [ 109.909571, -7.202619 ] } },
+{ "type": "Feature", "properties": { "name": "Masjid Nurul Ummah", "desc": null, "X": null, "Y": null, "Info": "public facilities" }, "geometry": { "type": "Point", "coordinates": [ 109.937333, -7.235375 ] } }
+]
+}
